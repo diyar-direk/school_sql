a643551d1b7b77102dd89b95a14102756735a508
diyar-direk <diyardireki111@gmail.com>
2025-11-03 00:31:00 -0800

    s

--- src/pages/quizes/router.js
+++ src/pages/quizes/router.js
@@ -1,5 +1,7 @@
 import { lazy } from "react";
 import { pagesRoute } from "../../constants/pagesRoute";
+import AllowedTo from "../../components/AllowedTo";
+import { roles } from "../../constants/enums";
 const AllQuizes = lazy(() => import("./AllQuizes"));
 const AddQuiz = lazy(() => import("./AddQuiz"));
 export const quizeRouter = [
@@ -9,6 +11,10 @@ export const quizeRouter = [
   },
   {
     path: pagesRoute.quize.add,
-    element: <AddQuiz />,
+    element: (
+      <AllowedTo roles={[roles.admin, roles.teacher]}>
+        <AddQuiz />
+      </AllowedTo>
+    ),
   },
 ];
